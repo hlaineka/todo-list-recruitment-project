de7d6d45fd11311e1c4e46f057457c2b35326fd9
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-06-24 14:32:59 +0300

    stuff made in the interview

--- src/ReactApp/app.jsx
+++ src/ReactApp/app.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import MockApi from '../MockApi'
+import PropTypes from 'prop-types';
 
-const App = () => <p>React app works!</p>;
+
+const api = new MockApi();
+
+const Todo = ({todo}) => (
+  <li>
+    <i className={todo.icon}/>
+    <div>{todo.task}</div>
+    <div>{todo.description}</div>
+  </li>
+)
+
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    task: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    done: PropTypes.bool.isRequired,
+    icon: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const App = () =>  { 
+  const [ todos, setTodos] = useState([])
+
+  useEffect(() => {
+    api.getTodos()
+    .then(response => response.json())
+    .then(initialTodos => {
+      setTodos(initialTodos)
+    })
+  })
+  return (
+  <ul>
+    {todos.map((todo) => <Todo todo={todo}/>)}
+  </ul>
+)}
 
 export default App;
